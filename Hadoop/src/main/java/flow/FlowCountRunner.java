8617896bebb8f523e70b5fb004f66e93a9106911
wangn <3579677768@qq.com>
2018-05-31 10:41:24 +0800

    update

--- Hadoop/src/main/java/flow/FlowCountRunner.java
+++ Hadoop/src/main/java/flow/FlowCountRunner.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 
 public class FlowCountRunner {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        System.setProperty("hadoop.home.dir", "G:\\winutils\\hadoop-2.8.3");
         Configuration configuration = new Configuration();
+
         Job job = Job.getInstance(configuration);
 
         job.setJarByClass(FlowCountRunner.class);
@@ -28,8 +30,11 @@ public class FlowCountRunner {
 
         job.setInputFormatClass(TextInputFormat.class);
 
-        FileInputFormat.setInputPaths(job, "hdfs://192.168.1.11:9000/in");
-        FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.1.11:9000/out/flowcount"));
+        /*FileInputFormat.setInputPaths(job, "hdfs://192.168.1.11:9000/in");
+        FileOutputFormat.setOutputPath(job, new Path("hdfs://192.168.1.11:9000/out/flowcount"));*/
+
+        FileInputFormat.setInputPaths(job, new Path("C:\\Users\\wangn\\Desktop\\a"));
+        FileOutputFormat.setOutputPath(job, new Path("C:\\Users\\wangn\\Desktop\\b"));
 
         job.waitForCompletion(true);
     }
