6c35c50cad7cf296c0257433fbd1089ff3514419
itning <itning@itning.top>
2018-09-30 00:55:37 +0800

    update

---
+++ Concurrent/src/main/java/voliatle/TestVolatile.java
@@ -0,0 +1,28 @@
+package voliatle;
+
+/**
+ * volatile 测试
+ *
+ * @author wangn
+ */
+@SuppressWarnings("all")
+public class TestVolatile {
+    private static volatile int num = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 100; i++) {
+                        // ++操作并不是原子操作
+                        num++;
+                    }
+                }
+            }).start();
+        }
+        Thread.sleep(2000);
+        //并不是线程安全
+        System.out.println(num);
+    }
+}
