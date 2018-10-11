d69a256f095bb59692c3d5c83fe99d0f78c4348b
itning <itning@itning.top>
2018-10-11 20:24:47 +0800

    update

---
+++ Kafka/src/main/java/top/itning/kafka/KafkaConsumerTest.java
@@ -0,0 +1,43 @@
+package top.itning.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * @author wangn
+ */
+public class KafkaConsumerTest {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.66.3:9092,192.168.66.4:9092,192.168.66.5:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // 自动commit
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        // 自动commit的间隔
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Collections.singletonList("first"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            if (records.isEmpty()) {
+                System.out.println("wait...");
+            }
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("partition = %d, topic = %s, offset = %d, key = %s, value = %s \n",
+                        record.partition(), record.topic(), record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
---
+++ Kafka/src/main/java/top/itning/kafka/KafkaProducerTest.java
@@ -0,0 +1,35 @@
+package top.itning.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * @author wangn
+ */
+public class KafkaProducerTest {
+    public static void main(String[] args) throws InterruptedException {
+        String topic = "first";
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.66.3:9092,192.168.66.4:9092,192.168.66.5:9092");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int messageNo = 1; messageNo < 100000; messageNo++) {
+            //5、调用producer的send方法发送数据
+            //注意：这里需要指定 partitionKey，用来配合自定义的MyLogPartitioner进行数据分发
+            Thread.sleep(1);
+            System.out.println("send: " + messageNo);
+            producer.send(new ProducerRecord<>(topic, messageNo+""));
+        }
+        producer.flush();
+        producer.close();
+    }
+}
