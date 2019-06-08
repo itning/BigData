e5f2d2ac1809a34800eb0d0a5090044b2125b441
itning <itning@itning.top>
2019-06-08 13:13:50 +0800

    cache

--- Guava/src/main/java/top/itning/bigdata/guava/caches/CachesTest.java
+++ Guava/src/main/java/top/itning/bigdata/guava/caches/CachesTest.java
@@ -1,14 +1,38 @@
 package top.itning.bigdata.guava.caches;
 
+import com.google.common.cache.*;
 import org.junit.Test;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.concurrent.TimeUnit;
+
 /**
+ * 缓存
  *
  * @author itning
  */
 public class CachesTest {
     @Test
-    public void test(){
+    public void test() {
+        LoadingCache<String, String> graphs = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .removalListener(new RemovalListener<String, String>() {
+                    @Override
+                    @ParametersAreNonnullByDefault
+                    public void onRemoval(RemovalNotification<String, String> notification) {
+                        System.out.println(notification);
+                    }
+                })
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    @ParametersAreNonnullByDefault
+                    public String load(String key) throws Exception {
+                        return null;
+                    }
+                });
 
+        Cache<String, String> cache = CacheBuilder.newBuilder()
+                .build();
     }
 }
