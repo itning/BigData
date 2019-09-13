a285c557431f16189cc0b2e2f499d1b5b962490c
itning <itning@itning.top>
2019-09-13 15:19:46 +0800

    update

--- Concurrent/src/main/java/threadlocal/ThreadLocalTest.java
+++ Concurrent/src/main/java/threadlocal/ThreadLocalTest.java
@@ -17,20 +17,25 @@ public class ThreadLocalTest {
         }
     };
 
+    private static ThreadLocal<Integer> integerThreadLocal = ThreadLocal.withInitial(() -> 1);
+
     public static void main(String[] args) {
         new ThreadTest().start();
         new ThreadTest().start();
         new ThreadTest().start();
         new ThreadTest().start();
         stringThreadLocal.set("main value");
-        System.out.println(stringThreadLocal.get());
-        stringThreadLocal.remove();
+        System.out.println(stringThreadLocal.get() + " " + integerThreadLocal.get());
+        //stringThreadLocal.remove();
     }
 
     static class ThreadTest extends Thread {
         @Override
         public void run() {
-            System.out.println(stringThreadLocal.get());
+            if ("Thread-0".equals(Thread.currentThread().getName())) {
+                integerThreadLocal.set(2);
+            }
+            System.out.println(stringThreadLocal.get() + " " + integerThreadLocal.get());
         }
     }
 }
