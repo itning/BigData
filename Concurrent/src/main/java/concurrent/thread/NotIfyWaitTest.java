b73ba49879ed19da80eb0abaefb6854bfef6be2d
itning <itning@itning.top>
2019-10-21 15:58:09 +0800

    update

--- Concurrent/src/main/java/concurrent/thread/NotIfyWaitTest.java
+++ Concurrent/src/main/java/concurrent/thread/NotIfyWaitTest.java
@@ -2,6 +2,9 @@ package concurrent.thread;
 
 /**
  * 输出 12A 34B 56C 78D 910E 1112F 1314G 1516H...5152Z
+ * 当线程执行wait()时，会把当前的锁释放，然后让出CPU，进入等待状态。
+ * 当执行notify/notifyAll方法时，会唤醒一个处于等待该 对象锁 的线程，
+ * 然后继续往下执行，直到执行完退出对象锁锁住的区域（synchronized修饰的代码块）后再释放锁。
  *
  * @author itning
  */
