6c42bab4af3cb1122fed38f88c81e7f8668fbacf
itning <ning.wang8@geely.com>
2020-11-02 14:17:38 +0800

    feat: easy rule

---
+++ EasyRule/src/main/java/top/itning/easyrule/EasyRuleApplication.java
@@ -0,0 +1,13 @@
+package top.itning.easyrule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRuleApplication.class, args);
+	}
+
+}
---
+++ EasyRule/src/test/java/top/itning/easyrule/EasyRuleApplicationTests.java
@@ -0,0 +1,13 @@
+package top.itning.easyrule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
